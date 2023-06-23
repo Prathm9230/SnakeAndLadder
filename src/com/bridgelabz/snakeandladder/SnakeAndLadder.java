cce82bf3f8010b10c121752c5c593b9aa904e44d
Prathm <jawahireprathmesh@gmail.com>
2023-06-23 12:55:39 +0530

    Ensure player gets exact winning position 100

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -5,14 +5,41 @@ import java.util.Random;
 
 public class SnakeAndLadder {
     public static int PLAYER = 0;
+    public static int TARGET = 100;
     public static void main(String[] args) {
-        {
-            //System.out.println("Welcome! to Snake And Ladder Game.");
-            Random random = new Random();
+        Random random = new Random();
+        Random c = new Random();
+        //UC-1
+        System.out.println("Welcome! to Snake And Ladder Game.");
+        for(int i=0;i<35;i++){
+            if(PLAYER==100)
+                break;
             int dice = random.nextInt(1,7);
-            System.out.println("Currently PLAYER is at postion:"+PLAYER);
+            int check=c.nextInt(3);
+            System.out.println("\nscore:"+PLAYER);
             System.out.println("Rolling the dice:"+dice);
+            PLAYER+=dice;
+            System.out.println("Currently PLAYER's score:"+PLAYER);
+            if(PLAYER==100)
+                break;
+            if(check == 0)
+            {
+                System.out.println("sorry! you got nothing.");
+                System.out.println("Current score of PLAYER:"+PLAYER);
+            }
+            else if(check == 1)
+            {
+                System.out.println("You have got a Ladder and you have to increase you score by :"+dice + " and play again");
+                PLAYER=PLAYER+dice;
+                System.out.println("Current score of is: "+PLAYER);
+                continue;
+            }
+            else  {
+                System.out.println("You have got a Snake and you have to increase you score by: "+dice);
+                PLAYER=PLAYER-dice;
+                System.out.println("Current score of is "+PLAYER);
 
+            }
         }
     }
 }
