8f91853578d1e5f5349aed860adb1bd8e1126121
Prathm <jawahireprathmesh@gmail.com>
2023-06-23 11:16:51 +0530

    Single player at 0 position

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -3,15 +3,11 @@ package com.bridgelabz.snakeandladder;
 import java.util.Random;
 
 public class SnakeAndLadder {
-    public static int PLAYER = 0;//single player at 0 position
+    public static int PLAYER = 0;
     public static void main(String[] args) {
         {
-            //System.out.println("Welcome! to Snake And Ladder Game.");
-            Random random = new Random();
+            System.out.println("Welcome! to Snake And Ladder Game.");
 
-            int dice = random.nextInt(1, 7);
-            System.out.println("Current position of user is: " + PLAYER);
-            System.out.println("Rolling the deice " + dice);
         }
     }
 }
