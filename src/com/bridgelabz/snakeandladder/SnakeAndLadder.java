b18134a4f87321c3aba6d7839c2b01897105a7bd
Prathm <jawahireprathmesh@gmail.com>
2023-06-23 11:26:09 +0530

    PLAYER rolls the dice to get random number btween 1 to 6

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,12 +1,17 @@
 package com.bridgelabz.snakeandladder;
 
+import java.sql.SQLOutput;
 import java.util.Random;
 
 public class SnakeAndLadder {
     public static int PLAYER = 0;
     public static void main(String[] args) {
         {
-            System.out.println("Welcome! to Snake And Ladder Game.");
+            //System.out.println("Welcome! to Snake And Ladder Game.");
+            Random random = new Random();
+            int dice = random.nextInt(1,7);
+            System.out.println("Currently PLAYER is at postion:"+PLAYER);
+            System.out.println("Rolling the dice:"+dice);
 
         }
     }
