d26c708d166740c246752aabe273c4bee9ec392f
Prathm9230 <133800729+Prathm9230@users.noreply.github.com>
2023-06-23 13:40:41 +0530

    Update SnakeAndLadder.java

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -41,5 +41,12 @@ public class SnakeAndLadder {
 
             }
         }
+         if ( PLAYER<TARGET){
+            System.out.println("\nComputer has Won the game");
+        }else {
+            System.out.println("\nYOU HAVE WON!!!" );
+        }
+
+        System.out.println("\nThe score of s: " + PLAYER);
     }
 }
